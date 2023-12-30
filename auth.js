6e553fd5c870fca558b40d88f8622d1c22475549
oceanicbliss <155108873+oceanicbliss@users.noreply.github.com>
2023-12-30 10:43:57 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,31 +1,61 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+
+
+
 const loginForm = document.getElementById("login-form");
     loginForm.addEventListener("submit", (event) => {
       event.preventDefault(); // Prevent default form submission
       // Get user input values
       const email = loginForm.email.value;
       const password = loginForm.password.value;
+      
       // Send credentials to an external service for validation
       // ...
+   
       
-      if (email === "leongpengjun@hotmail.com") {
-      var emailcheck = "1"
-    } else {
-      var emailcheck = "0"
-    }  
-    
-      if (password === "123") {
-      var passwordcheck = "1"
-    } else {
-      var passwordcheck = "0"
-    }
-    
-      if (emailcheck === "1" && passwordcheck === "1") {
-      var allcheck = true;
-    } else {
-      var allcheck = false;
-    }
-    
-      if (allcheck === true) {
+
+
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDrSvAt9G_ued4Yqu4dPwNMJrgIsxGH95Y",
+  authDomain: "oceanicbliss-c82af.firebaseapp.com",
+  projectId: "oceanicbliss-c82af",
+  storageBucket: "oceanicbliss-c82af.appspot.com",
+  messagingSenderId: "837993391350",
+  appId: "1:837993391350:web:db8710919a67f279f678d7",
+  measurementId: "G-3XQD6S74X8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+////////////////////////////////////////////////////////////////////////////////////
+///SIGNIN///
+
+let authenticate = false;
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+// Signed in 
+const user = userCredential.user;
+authenticate = true;
+//document.write(authenticate);
+//////////////////////////////////////////////////////////////////////////////////////////
+      // Respond to Firebase's Authentication
+      if (authenticate === true) {
         const logoutLink = document.createElement('a');
         logoutLink.href = 'index.html';
         logoutLink.textContent = 'LOGOUT';
@@ -49,13 +79,19 @@ const loginForm = document.getElementById("login-form");
         iframe.width = '1300';
         iframe.height = '500';
         document.body.appendChild(iframe);
-        
-      } else {
-        document.write("access denied");
-        setTimeout(denied, 1000);
-        function denied() {
-        window.location.href = 'index.html';
-        }
       }
-    
-    });
+// ...
+})
+.catch((error) => {
+const errorCode = error.code;
+const errorMessage = error.message;
+authenticate = false;
+document.write("ACCESS DENIED");
+setTimeout(denied, 1000);
+function denied() {
+  window.location.href = 'index.html';
+}
+});
+
+
+});
