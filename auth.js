6473a6cefebe9d5684d3b71704b5b3a4ed48b27a
oceanicbliss <155108873+oceanicbliss@users.noreply.github.com>
2023-12-31 08:36:59 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,7 +8,6 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 
 
 
-
 const loginForm = document.getElementById("login-form");
     loginForm.addEventListener("submit", (event) => {
       event.preventDefault(); // Prevent default form submission
@@ -52,33 +51,24 @@ signInWithEmailAndPassword(auth, email, password)
 // Signed in 
 const user = userCredential.user;
 authenticate = true;
+// Call onLogin function after successful authentication
+onLogin();
 //document.write(authenticate);
 //////////////////////////////////////////////////////////////////////////////////////////
       // Respond to Firebase's Authentication
       if (authenticate === true) {
-        const logoutLink = document.createElement('a');
-        logoutLink.href = 'index.html';
-        logoutLink.textContent = 'LOGOUT';
-        logoutLink.style.position = 'fixed';
-        logoutLink.style.top = '0px';
-        logoutLink.style.right = '5px';
-        document.body.appendChild(logoutLink);
-
-        //const title = document.createElement('b');
-        //title.textContent = 'Low Price Scanner';
-        //title.style.position = 'fixed';
-        //title.style.top = '65px';
-        //title.style.left = '5px';
-        //document.body.appendChild(title);
-  
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQkfdM0tANp3tURcIJnDdqqYnGB5OnAFIbvkFohs_YqMMK6jJH4fOuCGrfvoWZBlvl-ttEqelG6XwA8/pubhtml?widget=true&amp;headers=false';
-        iframe.style.position = 'fixed';
-        iframe.style.top = '100px';
-        iframe.style.left = '5px';
-        iframe.width = '1300';
-        iframe.height = '500';
-        document.body.appendChild(iframe);
+        
+        const title = document.createElement('b');
+        title.textContent = 'LOGGING IN....';
+        title.style.position = 'fixed';
+        title.style.top = '120px';
+        title.style.left = '50px';
+        title.style.color = 'red';
+        document.body.appendChild(title);
+        // Set a timeout to remove the title after 5 seconds
+        setTimeout(() => {
+        document.body.removeChild(title);
+        }, 5000);
       }
 // ...
 })
@@ -89,9 +79,93 @@ authenticate = false;
 document.write("ACCESS DENIED");
 setTimeout(denied, 1000);
 function denied() {
+  onLogout();
   window.location.href = 'index.html';
 }
 });
 
 
 });
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//COOKIES///
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        // Define the setCookie function
+        function setCookie(name, value, days) {
+          const expires = new Date();
+          expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+          document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+      }
+
+      // Define the getCookie function
+      function getCookie(name) {
+          const cookieArray = document.cookie.split(';');
+          for (let i = 0; i < cookieArray.length; i++) {
+              const cookie = cookieArray[i].trim();
+              if (cookie.startsWith(name + '=')) {
+                  return cookie.substring(name.length + 1);
+              }
+          }
+          return null;
+      }
+
+      // Example: Set a cookie when the user logs in
+      function onLogin() {
+          setCookie('loggedIn', 'true', 7); // Expires in 7 days
+      }
+
+      // Example: Set a cookie when the user logs out
+      function onLogout() {
+          setCookie('loggedIn', 'false', 7); // Expires in 7 days
+      }
+
+      // Example: Check if the user is logged in
+      function checkLoginStatus() {
+          const loggedIn = getCookie('loggedIn');
+          if (loggedIn === 'true') {
+              window.alert(loggedIn);
+          } else {
+              window.alert(loggedIn);
+          }
+          if (loggedIn === 'true') {
+            const logoutLink = document.createElement('a');
+            logoutLink.href = 'index.html';
+            logoutLink.textContent = 'LOGOUT';
+            logoutLink.style.position = 'fixed';
+            logoutLink.style.top = '0px';
+            logoutLink.style.right = '5px';
+            // Add a click event listener to the logout link
+            logoutLink.addEventListener("click", (event) => {
+            event.preventDefault(); // Prevent the default link behavior
+            // Call onLogout function when the link is clicked
+            onLogout();
+            // Additional logic for logging out, if needed
+            // Redirect to the logout URL
+            window.location.href = logoutLink.href;
+            });
+            document.body.appendChild(logoutLink);
+
+            // Render the iframe only if the user is logged in
+            const iframe = document.createElement('iframe');
+            iframe.src = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQkfdM0tANp3tURcIJnDdqqYnGB5OnAFIbvkFohs_YqMMK6jJH4fOuCGrfvoWZBlvl-ttEqelG6XwA8/pubhtml?widget=true&amp;headers=false';
+            iframe.style.position = 'fixed';
+            iframe.style.top = '100px';
+            iframe.style.left = '5px';
+            iframe.width = '1300';
+            iframe.height = '500';
+            document.body.appendChild(iframe);
+          }
+      }
+
+      
+      
+      //onLogin();
+      //onLogout();
+      checkLoginStatus();
+  
+      
+      
