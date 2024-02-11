4b14c0da9e0b1289129ba48129a7b5d264859157
oceanicbliss <oceanicblissb7@hotmail.com>
2024-02-11 09:00:08 +0700

    done

---
+++ FOLDER0_v1.4/test.js
@@ -0,0 +1,36 @@
+//Add the Firebase and Cloud Firestore libraries
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+//import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+//Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrSvAt9G_ued4Yqu4dPwNMJrgIsxGH95Y",
+  authDomain: "oceanicbliss-c82af.firebaseapp.com",
+  projectId: "oceanicbliss-c82af",
+  storageBucket: "oceanicbliss-c82af.appspot.com",
+  messagingSenderId: "837993391350",
+  appId: "1:837993391350:web:db8710919a67f279f678d7",
+  measurementId: "G-3XQD6S74X8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Read & Save Data
+let dataString = "";
+
+const querySnapshot = await getDocs(collection(db, "url1"));
+querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    Object.keys(data).forEach((key) => {
+        dataString += `${data[key]}, `;
+    });
+    dataString += "\n";
+});
+console.log(dataString);
